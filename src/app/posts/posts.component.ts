22df5210adfc42b73e09fd669e5d4ee78598110b
neo <ashrafuddin160@gmail.com>
2018-04-24 22:51:12 +0600

    adde a post method

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+
+export class PostsComponent implements OnInit {
+
+  posts : any[];
+
+  constructor(http : Http) { 
+    http.get('https://jsonplaceholder.typicode.com/posts').subscribe(response=>{
+      this.posts = response.json();
+    });
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
